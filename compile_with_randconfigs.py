67a5822c69ec7798953069a18f5574273ee4a6a0
s4nsec <s4nsec@gmail.com>
2024-02-27 12:55:00 -0500

    upload the initial script

---
+++ compile_with_randconfigs.py
@@ -0,0 +1,41 @@
+import argparse
+import subprocess
+import logging
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Generate random configurations for the kernel"
+    )
+    parser.add_argument(
+        "-k",
+        "--kernel_src",
+        type=str,
+        required=True,
+        help="Path to the kernel source directory",
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+def generate_randconfigs(kernel_src):
+    command = "KCONFIG_PROBABILTIY=10 make randconfig"
+
+    try:
+        result = subprocess.run(
+            command, shell=True, cwd=kernel_src, check=True, capture_output=True
+        )
+        logging.info(result.stdout)
+
+    except Exception as e:
+        logging.error(e)
+
+
+def main():
+    args = parse_args()
+    generate_randconfigs(args.kernel_src)
+
+
+if __name__ == "__main__":
+    main()
