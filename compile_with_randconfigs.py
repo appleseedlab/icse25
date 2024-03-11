3fc143c645d5e86e41c55200ed0ca7f55e925679
s4nsec <s4nsec@gmail.com>
2024-03-10 22:35:25 -0400

    append to a file instead of writing
    Previously I wrote content of each run to csv file by overwriting already existing content.
    This will just append to it, saving all the content.

--- compile_with_randconfigs.py
+++ compile_with_randconfigs.py
@@ -83,7 +83,7 @@ def git_clean(kernel_src):
 
 def save_to_csv(output_dir, seed, probability, config_path):
     with open(
-        f"{output_dir}/randconfig_experiment_results.csv", "w", newline=""
+        f"{output_dir}/randconfig_experiment_results.csv", "a", newline=""
     ) as csv_file:
         csvwriter = csv.writer(csv_file)
         csvwriter.writerow([seed, probability, config_path])
