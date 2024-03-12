40ccfb6f840e147e6a65be9571a96cbd42570dc5
s4nsec <s4nsec@gmail.com>
2024-03-12 11:57:34 -0400

    add docstrings to each function

--- compile_with_randconfigs.py
+++ compile_with_randconfigs.py
@@ -12,6 +12,10 @@ import requests
 
 
 def parse_args():
+    """
+    This function parses the command line arguments and returns them
+    Returns: parsed arguments
+    """
     parser = argparse.ArgumentParser(
         description="Generate random configurations for the kernel"
     )
@@ -71,6 +75,11 @@ def parse_args():
 
 
 def git_clean(kernel_src):
+    """
+    This function performs git clean on the provided kernel source
+    Args:
+    kernel_src: Path to the linux kernel source
+    """
     repo = git.Repo(kernel_src)
 
     untracked_files = repo.untracked_files
@@ -84,6 +93,14 @@ def git_clean(kernel_src):
 
 
 def save_to_csv(output_dir, seed, probability, config_path):
+    """
+    This function saves the seed, probability and config path to a csv file
+    Args:
+    output_dir: output directory
+    seed: seed used to generate the configuration
+    probability: probability used to generate the configuration
+    config_path: path to the generated configuration file
+    """
     with open(
         f"{output_dir}/randconfig_experiment_results.csv", "a", newline=""
     ) as csv_file:
@@ -91,7 +108,18 @@ def save_to_csv(output_dir, seed, probability, config_path):
         csvwriter.writerow([seed, probability, config_path])
 
 
-def generate_randconfigs(kernel_src, output_dir):
+def generate_randconfigs(kernel_src, output_dir) -> list:
+    """
+    This function generates random configurations for the kernel
+    It uses the randconfig make target to generate random configurations with seeds of current time and probability from 10 to 90
+    Then it copies the generated configuration files to the output directory
+    Later, it saves the seed, probability and config path to a csv file
+    Args:
+    kernel_src: path to the kernel source
+    output_dir: output directory
+    Returns: list of generated configuration files
+    Raises: Exception
+    """
     config_path = f"{kernel_src}/.config"
     generated_config_files = []
 
@@ -121,12 +149,31 @@ def generate_randconfigs(kernel_src, output_dir):
 
 
 def git_checkout_commit(kernel_src: str, commit_hash: str):
+    """
+    This function checks out to a commit hash in the git repository
+    Args:
+    kernel_src: path to the kernel source
+    commit_hash: commit hash to checkout to
+    """
     repo = git.Repo(kernel_src)
     repo.git.checkout(commit_hash)
     return repo.head.commit.hexsha
 
 
-def compile_kernel(kernel_src_list, generated_config_files, output_dir, commit_hash):
+def compile_kernel(
+    kernel_src_list, generated_config_files, output_dir, commit_hash
+) -> list:
+    """
+    This function compiles the kernel with the generated configuration files
+    It first git cleans the kernel source, then checks out to the commit hash
+    Then it builds the kernel with the generated configuration files
+    Args:
+    kernel_src_list: list of kernel source paths
+    generated_config_files: list of generated configuration files
+    output_dir: output directory
+    commit_hash: commit hash to checkout to
+    Returns: list of configuration files that failed to compile
+    """
     config_not_compiled = []
     for kernel_src, config_file in zip(kernel_src_list, generated_config_files):
         git_clean(kernel_src)
@@ -166,6 +213,16 @@ def compile_kernel(kernel_src_list, generated_config_files, output_dir, commit_h
 def prepare_syzkaller_configs(
     kernel_src: str, syzkaller_config_path: str, output_syzkaller_config_list: list
 ) -> tuple[list, list]:
+    """
+    This function prepares syzkaller configuration files for each kernel source
+    The function reads the default syzkaller configuration file template, modifies cpu, mem, kernel_obj, vm, http and workdir fields for each syzkaller session
+    It also generates paths to linux kernel source files for each syzkaller session
+    Args:
+    kernel_src: path to the linux kernel source
+    syzkaller_config_path: path to the syzkaller configuration file
+    output_syzkaller_config_list: list of output syzkaller configuration files
+    Returns: list of kernel source paths, list of syzkaller configuration files
+    """
     kernel_src_list = []
     kernel_src_list.append(kernel_src)
 
@@ -207,14 +264,23 @@ def prepare_syzkaller_configs(
 
 
 def run_syzkaller_in_parallel(
-    syzkaller_config_list: list, timeout_duration: str = "1"
+    syzkaller_config_list: list,
+    output_dir: str,
+    timeout_duration: str = "1",
 ) -> list:
+    """
+    This function runs multiple syzkaller instances in parallel and saves their output to a log file
+    Args:
+    syzkaller_config_list: list of syzkaller configuration files
+    output_dir: output directory to write the log files
+    Returns: list of tmux sessions
+    """
     logging.debug("Running syzkaller in parallel")
     tmux_sessions_list = []
 
     for syzkaller_config in syzkaller_config_list:
         syzkaller_path = "/home/sanan/Documents/syzkaller"
-        command = f"cd {syzkaller_path}; ./bin/syz-manager -config={syzkaller_config}"
+        command = f"cd {syzkaller_path}; ./bin/syz-manager -config={syzkaller_config} 2>&1 | tee {output_dir}/{os.path.basename(syzkaller_config)}.log"
 
         tmux_session_name = syzkaller_config.replace(".", "_")
 
@@ -285,6 +351,13 @@ def run_syzkaller_in_parallel(
 def timeout_to_download_html(
     timeout_duration: int, saved_coverage_file_path: str, config_path_list: list
 ):
+    """
+    This function waits for the timeout duration and then downloads the coverage file for each syzkaller session.
+    Args:
+    timeout_duration: timeout duration
+    saved_coverage_file_path: path to save the coverage file
+    config_path_list: list of syzkaller configuration files
+    """
     timeout_duration_hours = int(timeout_duration) * 60
     buffer_time = 60
 
@@ -301,6 +374,12 @@ def timeout_to_download_html(
 
 
 def terminate_syzkaller_sessions(tmux_sessions_list: list):
+    """
+    This function terminates the syzkaller sessions
+    args:
+    tmux_sessions_list: list of tmux sessions
+    Raises: Exception
+    """
     for session in tmux_sessions_list:
         try:
             result = subprocess.run(
@@ -313,7 +392,16 @@ def terminate_syzkaller_sessions(tmux_sessions_list: list):
             logging.error(f"tmux errored out with exception: {e}")
 
 
-def download_html(saved_coverage_file_path, coverage_file_name, coverage_port):
+def download_html(
+    saved_coverage_file_path: str, coverage_file_name: str, coverage_port: int
+):
+    """
+    This function downloads the coverage file from the syzkaller manager
+    args:
+    saved_coverage_file_path: path to save the coverage file
+    coverage_file_name: name of the coverage file
+    coverage_port: port number of the syzkaller manager
+    """
     url = f"http://127.0.0.1:{coverage_port}/rawcover"
     response = requests.get(url)
 
@@ -351,7 +439,9 @@ def main():
     # for i in range(len(kernel_src_list)):
     #     logging.debug(f"Kernel src: {kernel_src_list[i]}")
 
-    tmux_sessions_list = run_syzkaller_in_parallel(syzkaller_config_list)
+    tmux_sessions_list = run_syzkaller_in_parallel(
+        syzkaller_config_list, args.output_dir
+    )
     logging.debug(f"Tmux sessions list: {tmux_sessions_list}")
 
     timeout_to_download_html(args.timeout, args.output_dir, syzkaller_config_list)
