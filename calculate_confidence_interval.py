8ff2fb218af7e98e3eaeccceaa21ba53a29cc33d
s4nsec <s4nsec@gmail.com>
2024-03-18 21:12:42 -0400

    add a function to calculate mean and confidence of the provided data

--- calculate_confidence_interval.py
+++ calculate_confidence_interval.py
@@ -30,3 +30,11 @@ def calculate_std(data):
     return np.std(data)
 
 
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0 * np.array(data)
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t.ppf((1 + confidence) / 2.0, n - 1)
+    return m, m - h, m + h
+
+
