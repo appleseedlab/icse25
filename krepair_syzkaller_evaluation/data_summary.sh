1c4686beb39c8102e4689f17d9c36c0e26e39b4b
s4nsec <s4nsec@gmail.com>
2024-03-20 21:57:23 -0400

    remove build time related code

--- krepair_syzkaller_evaluation/data_summary.sh
+++ krepair_syzkaller_evaluation/data_summary.sh
@@ -10,9 +10,9 @@ cat ${coverable_patches} | while read commit; do
 	configdir=${commitdir}/syzkaller_config/results
 	echo -n ${commit},
 	echo -n syzkaller_config,
-	echo -n $(cat ${commitdir}/syzkaller_config/results/build.errcode),
+	# echo -n $(cat ${commitdir}/syzkaller_config/results/build.errcode),
 	echo -n $(python3 ${scriptsdir}/patch_coverage.py ${configdir}/koverage_outfile | cut -d' ' -f4),
-	echo -n $(cat ${commitdir}/syzkaller_config/results/build.time | head -n1)
+	# echo -n $(cat ${commitdir}/syzkaller_config/results/build.time | head -n1)
 	echo
 done
 
@@ -22,15 +22,15 @@ cat ${coverable_patches} | while read commit; do
 	echo -n ${commit},
 	echo -n krepair,
 	if [[ -e ${configdir}/patch_covered ]]; then
-		echo -n $(cat ${commitdir}/syzkaller_config/results/build.errcode),
+		# echo -n $(cat ${commitdir}/syzkaller_config/results/build.errcode),
 		echo -n $(python3 ${scriptsdir}/patch_coverage.py ${configdir}/koverage_outfile | cut -d' ' -f4),
-		echo -n $(cat ${commitdir}/syzkaller_config/results/build.time | head -n1)
+		# echo -n $(cat ${commitdir}/syzkaller_config/results/build.time | head -n1)
 	else
 		if [[ -e ${configdir}/krepair_one ]]; then
-			builderrcode=$(cat ${commitdir}/syzkaller_config/results/repaired_build.errcode)
-			echo -n ${builderrcode},
+			# builderrcode=$(cat ${commitdir}/syzkaller_config/results/repaired_build.errcode)
+			# echo -n ${builderrcode},
 			echo -n $(python3 ${scriptsdir}/patch_coverage.py ${configdir}/repaired_koverage_outfile | cut -d' ' -f4),
-			echo -n $(cat ${commitdir}/syzkaller_config/results/repaired_build.time | head -n1)
+			# echo -n $(cat ${commitdir}/syzkaller_config/results/repaired_build.time | head -n1)
 		else
 			echo -n n/a,
 			ls ${configdir}/repaired_koverage_outfile.* >/dev/null 2>/dev/null
