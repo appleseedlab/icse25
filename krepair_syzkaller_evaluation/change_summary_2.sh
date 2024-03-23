5a0a938322e5fa5d0fda64a97a67b071185d50aa
s4nsec <s4nsec@gmail.com>
2024-03-22 22:37:27 -0400

    rename variables

--- krepair_syzkaller_evaluation/change_summary_2.sh
+++ krepair_syzkaller_evaluation/change_summary_2.sh
@@ -7,7 +7,7 @@ echo "commit,configfile,changes"
 while IFS=, read -r commit configfile repairedconfig; do
 	changepct=0
 	repairedconfig=$(echo ${repairedconfig} | tr -d '\r')
-	changes=$(python3 /home/sanan/opt/icse25/krepair_syzkaller_evaluation/measure_change.py --original-config /home/sanan/research/syzbot_configuration_files/${configfile} /home/sanan/research/repaired_configuration_files/${repairedconfig} 2>/dev/null | jq .repaired[].change_wrt_original | paste -sd+ | bc -lq)
+	changes=$(python3 /home/anon/opt/icse25/krepair_syzkaller_evaluation/measure_change.py --original-config /home/anon/research/syzbot_configuration_files/${configfile} /home/anon/research/repaired_configuration_files/${repairedconfig} 2>/dev/null | jq .repaired[].change_wrt_original | paste -sd+ | bc -lq)
 	echo -n ${configfile},
 	echo -n ${repairedconfig},
 	echo -n ${changes}
