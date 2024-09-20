7e29643c3999d7d301ad735845fcea4acfd2585a
s4nsec <s4nsec@gmail.com>
2024-09-20 15:07:57 -0400

    Add compressed files containing all guild bugs and reproducers

--- repairer_script/collect_reproducers.sh
+++ repairer_script/collect_reproducers.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+if [[ $# -ne 3 ]]; then
+    echo "[*] Usage: ./program <path-to-repaired-reproducers-directory> <path-to-csv-containing-paths-to-repaired-reproducers> <repaired_bugs_directory>"
+    echo "[*] Example: ./collec_reproducers.sh ./repaired_reproducers/ ./repaired_reproducers.csv"
+    exit 1
+fi
+
+repaired_reproducers=$1
+repaired_reproducers_csv=$2
+repaired_bugs=$3
+
+c_reproducers="$repaired_reproducers/c_reproducers"
+syz_reproducers="$repaired_reproducers/syz_reproducers"
+
 # Create the directories if they don't exist
-mkdir -p /home/anon/research/repaired_reproducers/c_reproducers
-mkdir -p /home/anon/research/repaired_reproducers/syz_reproducers
+mkdir -p $c_reproducers
+mkdir -p $syz_reproducers
+
+# Replace "/home/sanan/research/guild_bugs" with the new folder
+cp "$repaired_reproducers_csv" "${repaired_reproducers_csv}.bak"
+sed -i "s|/home/sanan/research/guild_bugs|$repaired_bugs|g" "$repaired_reproducers_csv"
 
 # Read the CSV file line by line
 while IFS=, read -r description type location; do
@@ -11,20 +28,24 @@ while IFS=, read -r description type location; do
 		continue
 	fi
 
+    folder_name=$(echo "$location" | sed "s|$repaired_bugs||" | sed 's|/crashes/.*||')
+
 	# Determine the type and copy the file to the appropriate folder
 	if [ "$type" == "C reproducer" ]; then
-		cp "$location/repro.cprog" "/home/anon/research/repaired_reproducers/c_reproducers/"
+		cp "$location/repro.cprog" "$c_reproducers/${folder_name}_repro.cprog"
 	elif [ "$type" == "Syz reproducer" ]; then
 		if [ -f "$location/repro.prog" ]; then
-			cp "$location/repro.prog" "/home/anon/research/repaired_reproducers/syz_reproducers/"
+			cp "$location/repro.prog" "$syz_reproducers/${folder_name}_repro.prog"
 		elif [ -f "$location/repro0" ]; then
-			cp "$location/repro0" "/home/anon/research/repaired_reproducers/syz_reproducers/"
+			cp "$location/repro0" "$syz_reproducers/${folder_name}_repro.prog"
 		elif [ -f "$location/repro" ]; then
-			cp "$location/repro" "/home/anon/research/repaired_reproducers/syz_reproducers/"
+			cp "$location/repro" "$syz_reproducers/${folder_name}_repro.prog"
 		else
 			echo "No Syz reproducer found in $location"
 		fi
 	else
 		echo "Unknown type: $type"
 	fi
-done <repaired_reproducers.csv
+done <$repaired_reproducers_csv
+
+cp "${repaired_reproducers_csv}.bak" "$repaired_reproducers_csv"
