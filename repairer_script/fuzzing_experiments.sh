bb40bbeaed1f4d036c8e2031e265545914ba7212
s4nsec <s4nsec@gmail.com>
2024-12-17 15:11:40 +0400

    Update fuzzing_experiments.sh to account for both default and repaired experiments

--- repairer_script/fuzzing_experiments.sh
+++ repairer_script/fuzzing_experiments.sh
@@ -3,22 +3,31 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(realpath "$SCRIPT_DIR/..")"
 
-if [ "$#" -ne 6 ]; then
-    echo "[!] Usage: ./program <csv-file> <path to linux-next> <path to syzkaller>
+# check experiment type
+if [ "$1" != "repaired" ] && [ "$1" != "default" ]; then
+    echo "[!] Please specify the experiment type: repaired or default"
+    exit 1
+fi
+
+if [ "$#" -ne 7 ]; then
+    echo "[!] Usage: ./program <experiment_type> <csv-file> <path to linux-next> <path to syzkaller>
     <path to debian image> <path to syzbot configs> <path to output folder>"
-    echo "[*] Example Usage: ./program repairer_script/fuzzing_parameters.csv " \
+    echo "[*] Example Usage: ./program default repairer_script/fuzzing_parameters.csv " \
         "~/linux-next/ ~/syzkaller/ ~/debian_images/" \
         "camera_ready/configuration_files/syzbot_configuration_files ~/output/"
     echo "[-] Exiting..."
     exit 9
 fi
 
-csv_file=$REPO_ROOT/$1
-dir_linux_next=$2
-syzkaller_path=$3
-debian_image_path=$4
-syzbot_config_files_path=$REPO_ROOT/$5
-output_path=$6
+experiment_type=$1
+csv_file=$REPO_ROOT/$2
+dir_linux_next=$3
+syzkaller_path=$4
+debian_image_path=$5
+syzbot_config_files_path=$REPO_ROOT/$6
+output_path=$7
+
+repaired_config_files_path="$REPO_ROOT/camera_ready/configuration_files/repaired_configuration_files"
 
 unix_time=$(date +%s)
 output_path=$output_path/$unix_time
@@ -27,10 +36,25 @@ log_file=$output_path/main_script_logs.log
 
 exec > >(tee -i "$log_file") 2>&1
 
+if [ "$experiment_type" == "repaired" ]; then
+    # Path to saved diff files that are used
+    output_of_diff=$output_path/coverage_commit_diff_files/;
+    mkdir -p $output_of_diff
+
+    # Path to saved koverage result files
+    output_of_koverage=$output_path/coverage_commit_koverage_files/;
+    mkdir -p $output_of_koverage
+
+    # Path to saved repaired koverage result files
+    output_of_repaired_koverage_path=$output_path/coverage_commit_configuration_files/;
+    mkdir -p $output_of_repaired_koverage_path
+fi
+
 if [ ! -f "$csv_file" ]; then
     echo "[-] The csv file does not exist: $csv_file"
     exit 1
 fi
+echo "[+] Experiment type: $experiment_type"
 echo "[+] CSV file: $csv_file"
 
 if [ ! -d "$dir_linux_next" ]; then
@@ -64,7 +88,7 @@ done
 
 echo "[*] Syzkaller port: $syzkaller_port"
 
-while IFS=, read -r commit_hash syzbot_config_name git_tag; do
+while IFS=, read -r commit_hash syzbot_config_name git_tag repaired_config_name; do
     echo "[+] Read Config Name: $syzbot_config_name"
     echo "[+] Read Commit Hash: $commit_hash"
     echo "[+] Syzkaller port: $syzkaller_port"
@@ -89,32 +113,27 @@ while IFS=, read -r commit_hash syzbot_config_name git_tag; do
         echo "[-] The syzbot config file does not exist: ${syzbot_config_files_path}/${syzbot_config_name}"
         exit 1
     fi
-    cp ${syzbot_config_files_path}/${syzbot_config_name} $dir_linux_next
 
     echo "[+] Starting making defconfig..."
     make defconfig
 
+    if [ "$1" == "default" ]; then
+        echo "[+] Copying the syzbot config file to .config"
+        cp ${syzbot_config_files_path}/${syzbot_config_name} $dir_linux_next/.config
+    elif [ "$1" == "repaired" ]; then
+        echo "[+] Copying the repaired config file to .config"
+        cp ${repaired_config_files_path}/${repaired_config_name} .config
+    fi
+
     echo "[+] Made defconfig. Now making kvm_guest.config..."
-
-    cp ${syzbot_config_name} .config
-
     make kvm_guest.config
 
     echo "[+] Made kvm_guest.config"
 
     echo "[+] Adding default syzkaller configs to .config file"
     # add syzkaller default config options
-    cat >> .config <<EOF
-    CONFIG_KCOV=y
-    CONFIG_DEBUG_INFO=y
-    CONFIG_DEBUG_INFO_DWARF4=y
-    CONFIG_KASAN=y
-    CONFIG_KASAN_INLINE=y
-    CONFIG_CONFIGFS_FS=y
-    CONFIG_SECURITYFS=y
-    CONFIG_CMDLINE_BOOL=y
-    CONFIG_CMDLINE="net.ifnames=0"
-EOF
+    ./scripts/config --enable CONFIG_KCOV --enable CONFIG_DEBUG_INFO --enable CONFIG_DEBUG_INFO_DWARF4 --enable CONFIG_KASAN --enable CONFIG_KASAN_IN
+LINE --enable CONFIG_CONFIGFS_FS --enable CONFIG_SECURITYFS --enable CONFIG_CMDLINE_BOOL --set-val CONFIG_CMDLINE "net.ifnames=0"
 
     echo "[+] Making olddefconfig..."
     make olddefconfig
@@ -129,7 +148,15 @@ EOF
     echo "[+] Created workdir ${workdir_name} for the current session"
     echo "[+] Creating new config file for syzkaller config"
 
-    cat > "$syzkaller_path/my.cfg" <<EOF
+    # check if syz-manager config file exists,
+    # if not, create a new one
+    if [ ! -f "$REPO_ROOT/$syzkaller_path/my.cfg" ]; then
+        echo "[-] The syzkaller config file does not exist: $syzkaller_path/my.cfg"
+        echo "[+] Creating a new syzkaller config file"
+        touch "$REPO_ROOT/$syzkaller_path/my.cfg"
+    fi
+
+    cat > "$REPO_ROOT/$syzkaller_path/my.cfg" <<EOF
     {
       "target": "linux/amd64",
       "http": "127.0.0.1:$syzkaller_port",
@@ -137,7 +164,7 @@ EOF
       "kernel_obj": "$dir_linux_next",
       "image": "$debian_image_path/bullseye.img",
       "sshkey": "$debian_image_path/bullseye.id_rsa",
-      "syzkaller": "$syzkaller_path",
+      "syzkaller": "$REPO_ROOT/$syzkaller_path",
       "procs": 8,
       "type": "qemu",
       "vm": {
@@ -153,7 +180,7 @@ EOF
     fuzzing_instance_log_path="$output_path/fuzzing_instance_logs/syzkaller_terminal_${syzbot_config_name}_${commit_hash}.log"
 
     echo "[+] Writing logs to ${fuzzing_instance_log_path}"
-    timeout 12h $syzkaller_path/bin/syz-manager -config=$syzkaller_path/my.cfg 2>&1 | tee ${fuzzing_instance_log_path};
+    timeout 12h "$REPO_ROOT/$syzkaller_path/bin/syz-manager" -config=$REPO_ROOT/$syzkaller_path/my.cfg 2>&1 | tee ${fuzzing_instance_log_path};
     exit_status_timeout=${PIPESTATUS[0]}
 
     if [ $exit_status_timeout -eq 0 ]; then
