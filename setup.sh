1b6245cf662782f2dea9d431ed6bdf68c58206c5
s4nsec <s4nsec@gmail.com>
2024-12-20 18:04:11 +0400

    Make the script noninteractive

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+export DEBIAN_FRONTEND=noninteractive
+
 # install go
 wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.23.4.linux-amd64.tar.gz
