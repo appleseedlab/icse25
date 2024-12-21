fe9fae7b7b54cdebfd1d77455c06e69c1562894c
s4nsec <s4nsec@gmail.com>
2024-12-21 04:46:11 +0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,30 +24,34 @@ else
     rm $GOTAR
 fi
 
-sudo apt install make libncurses-dev
-
-# install qemu
-sudo apt install qemu-system-x86
+echo "[+] Installing dependencies"
+sudo apt update
+sudo apt install -y build-essentials make libncurses-dev qemu-system-x86 debootstrap python3 python3-pip git-lfs git
 
 # install syzkaller
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(realpath "$SCRIPT_DIR/")"
 
 SYZKALLER_SRC="$REPO_ROOT/syzkaller"
+
+echo "[+] Installing syzkaller"
 (cd $SYZKALLER_SRC; git submodule update --init --recursive; make;)
 
 # create a debian image
 export IMAGE=./debian_image
-sudo apt install debootstrap
 mkdir -p $REPO_ROOT/$IMAGE
+
+echo "[+] Creating a debian image"
 (cd $IMAGE; wget https://raw.githubusercontent.com/google/syzkaller/master/tools/create-image.sh -O create-image.sh)
 (cd $IMAGE; chmod +x create-image.sh)
 (cd $IMAGE; ./create-image.sh)
 
 # install gdown
+echo "[+] Installing gdown"
 pip3 install gdown --break-system-packages
 
 # download linux-next
+echo "[+] Downloading linux-next"
 gdown --id 1H_aNBlJZ9qBLF0gvOflBE3-rou0EEbmT
 
 # download reproducer files with git-lfs
