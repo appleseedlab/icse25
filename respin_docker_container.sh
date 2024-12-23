1c9d9a9a2aff381f0e0502f106f2bf55542dad6a
s4nsec <s4nsec@gmail.com>
2024-12-23 18:04:42 +0400

    Remove all previous docker containers to save up some space

--- respin_docker_container.sh
+++ respin_docker_container.sh
@@ -6,5 +6,7 @@ image_name=$2
 docker stop $container_name
 docker rm $container_name
 
+docker system prune -f
+
 docker build . -t $image_name
 docker run -dit --name $container_name $image_name bash
