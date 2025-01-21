dc71d7bdae393ea153f92315756d12ada5caebeb
s4nsec <s4nsec@gmail.com>
2025-01-20 22:37:59 -0500

    Update get_config_times.sh
    - Parametrize resources
    - Force and quiet checkout

--- experiments/RQ2/table4/original/get_config_times.sh
+++ experiments/RQ2/table4/original/get_config_times.sh
@@ -5,22 +5,31 @@ set -x
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(realpath "$SCRIPT_DIR/../../../../")"
 
-KERNEL_SRC="$REPO_ROOT/linux-next"
+default_kernel_src="$REPO_ROOT/linux-next"
+
+KERNEL_SRC=${1:-$default_kernel_src}
+KERNEL_SRC="$(realpath $KERNEL_SRC)"
+
 # exit if kernel source directory does not exist
 if [ ! -d "$KERNEL_SRC" ]; then
     echo "[-] Kernel source directory does not exist. Please provide the correct path."
     exit 1
 fi
 
-SRC_CSV_FILE="$SCRIPT_DIR/../repaired_configs.csv"
-CONFIGS_DIR="$REPO_ROOT/configuration_files/syzbot_configuration_files"
+default_src_csv_file="$SCRIPT_DIR/../repaired_configs.csv"
+SRC_CSV_FILE=${2:-$default_src_csv_file}
+SRC_CSV_FILE="$(realpath $SRC_CSV_FILE)"
+
+default_configs_dir="$REPO_ROOT/configuration_files/syzbot_configuration_files"
+CONFIGS_DIR=${3:-$default_configs_dir}
+CONFIGS_DIR="$(realpath $CONFIGS_DIR)"
 
 # Read config_name, kernel_id, commit_id from csv file
 
 while IFS=, read -r commit_id config_name kernel_id
 do
     # checkout to the kernel_id
-    (cd $KERNEL_SRC; git checkout $kernel_id)
+    (cd $KERNEL_SRC; git -f checkout $kernel_id -q)
 
     # make defconfig
     (cd $KERNEL_SRC; make defconfig)
